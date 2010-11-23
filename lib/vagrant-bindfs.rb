e127f89bd7c6eb2511a98c6d53f70ac62b38a741
Gaël-Ian <gaelian.havard@gmail.com>
2010-11-23 03:42:20 +0100

    Comments and README update

--- lib/vagrant-bindfs.rb
+++ lib/vagrant-bindfs.rb
@@ -2,8 +2,8 @@ require 'vagrant'
 require 'vagrant-bindfs/config'
 require 'vagrant-bindfs/middleware'
 
-# Insert Bindfs at the end of the "up" Action stack
+# Insert Bindfs before the NFS middleware in the ":start" Action stack
 Vagrant::Action[:start].insert Vagrant::Action::VM::NFS, VagrantBindfs::Middleware
 
-# Add our custom translations to the load path
+# Add custom translations to the load path
 I18n.load_path << File.expand_path("../../locales/en.yml", __FILE__)
