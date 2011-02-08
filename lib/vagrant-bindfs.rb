9c3d76a0dd160ac0cc2c3990032b24bebaed792e
Folken Laëneck <folken.laeneck@gmail.com>
2011-02-08 02:34:24 +0100

    Middleware should be inserted in the :up stack too to allow use of bindfs as soon as the VM is created (not only on subsequents boots)

--- lib/vagrant-bindfs.rb
+++ lib/vagrant-bindfs.rb
@@ -2,8 +2,9 @@ require 'vagrant'
 require 'vagrant-bindfs/config'
 require 'vagrant-bindfs/middleware'
 
-# Insert Bindfs before the NFS middleware in the ":start" Action stack
+# Insert Bindfs before the NFS middleware in the ":start" and ":up" Action stack
 Vagrant::Action[:start].insert Vagrant::Action::VM::NFS, VagrantBindfs::Middleware
+Vagrant::Action[:up].insert Vagrant::Action::VM::NFS, VagrantBindfs::Middleware
 
 # Add custom translations to the load path
 I18n.load_path << File.expand_path("../../locales/en.yml", __FILE__)
