570748937280a8bc5f4b003ae67d8ed9e567d111
Andrew Noruk <andrew.noruk@milodigital.com>
2015-01-16 15:28:28 -0800

    use version suffix in source directory path

--- lib/vagrant-bindfs/cap/redhat/bindfs_install.rb
+++ lib/vagrant-bindfs/cap/redhat/bindfs_install.rb
@@ -6,11 +6,14 @@ module VagrantPlugins
 
           def self.bindfs_install(machine)
             machine.communicate.tap do |comm|
+
+              require "vagrant-bindfs/version"
+
               if comm.test("grep \"CentOS release 6\" /etc/centos-release")
                 comm.sudo("yum -y install fuse fuse-devel")
                 comm.sudo("wget http://bindfs.org/downloads/bindfs-#{VagrantPlugins::Bindfs::SOURCE_VERSION}.tar.gz -O bindfs.tar.gz")
                 comm.sudo("tar --overwrite -zxvf bindfs.tar.gz")
-                comm.sudo("[ -d ./bindfs ] && cd bindfs && ./configure && make && make install")
+                comm.sudo("[ -d ./bindfs-#{VagrantPlugins::Bindfs::SOURCE_VERSION} ] && cd bindfs-#{VagrantPlugins::Bindfs::SOURCE_VERSION} && ./configure && make && make install")
                 comm.sudo("ln -s /usr/local/bin/bindfs /usr/bin")
               else
                 comm.sudo("yum -y install bindfs")
