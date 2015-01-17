38c5b299e8dbbb9b4abc3f27afd4b740a518b1bb
Gaël-Ian <gaelian.havard@gmail.com>
2015-01-17 11:59:18 +0100

    Automatically add fuse to /etc/modules if not loaded at startup. Close #20
    Now add automatically 'fuse' to the list of startup loaded kernel module in
    /etc/modules so the modprobe step will be skipped on next boot.
    
    As Ubuntu (and others ?) seems to not have any 'fuse' kernel module to load,
    double check is done after lsmod to see if the module is listed in /etc/modules
    and should have been loaded at startup.

--- lib/vagrant-bindfs/cap/linux/bindfs_installed.rb
+++ lib/vagrant-bindfs/cap/linux/bindfs_installed.rb
@@ -9,10 +9,13 @@ module VagrantPlugins
           end
 
           def self.loaded_fuse?(machine)
-            machine.communicate.test("lsmod | grep -q fuse")
+            machine.communicate.test("lsmod | grep -q fuse || grep -q fuse /etc/modules")
           end
 
           def self.modprobe_fuse(machine)
+            unless machine.communicate.test("grep -q fuse /etc/modules")
+              machine.communicate.sudo("bash -c \"echo 'fuse' >> /etc/modules\"")
+            end
             machine.communicate.sudo("/sbin/modprobe fuse")
           end
 
