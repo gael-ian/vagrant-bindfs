83a1848cecc5bdd1ad1aad39cba4986b98f58b09
Gaël-Ian Havard <gaelian.havard@gmail.com>
2016-07-01 16:40:55 +0200

    Rollback to bindfs.org to download source tarballs

--- lib/vagrant-bindfs/version.rb
+++ lib/vagrant-bindfs/version.rb
@@ -4,7 +4,7 @@ module VagrantPlugins
     VERSION         = "0.4.7"
     
     SOURCE_VERSION  = "1.13.1"
-    SOURCE_URL      = "https://github.com/mpartel/bindfs/archive/#{SOURCE_VERSION}.tar.gz"
+    SOURCE_URL      = "http://bindfs.org/downloads/bindfs-#{SOURCE_VERSION}.tar.gz"
     
   end
 end
