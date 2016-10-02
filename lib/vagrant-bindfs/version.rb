8c9d991a8230e8d285aef27138db138de3ac4cd0
Gaël-Ian Havard <gaelian.havard@gmail.com>
2016-10-02 19:40:13 +1300

    Bump to version 0.4.10

--- lib/vagrant-bindfs/version.rb
+++ lib/vagrant-bindfs/version.rb
@@ -1,9 +1,9 @@
 module VagrantPlugins
   module Bindfs
     
-    VERSION         = "0.4.9"
+    VERSION         = "0.4.10"
     
-    SOURCE_VERSION  = "1.13.1"
+    SOURCE_VERSION  = "1.13.2"
     SOURCE_URLS     = [
       "http://bindfs.org/downloads/bindfs-#{SOURCE_VERSION}.tar.gz",
       "http://bindfs.dnsalias.net/downloads/bindfs-#{SOURCE_VERSION}.tar.gz"
