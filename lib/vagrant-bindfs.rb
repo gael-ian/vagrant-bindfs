d2f7dbfb783e2c8a13375ce097fb4558b4030282
Gaël-Ian Havard <gaelian.havard@gmail.com>
2016-11-29 02:04:57 +1300

    Simplify translations loading

--- lib/vagrant-bindfs.rb
+++ lib/vagrant-bindfs.rb
@@ -4,21 +4,14 @@ rescue LoadError
   raise "The Vagrant bindfs plugin must be run within Vagrant"
 end
 
-require "pathname"
-
 module VagrantBindfs
 
   autoload :Vagrant,      "vagrant-bindfs/vagrant"
   autoload :Bindfs,       "vagrant-bindfs/bindfs"
 
-    
-  def self.source_root
-    @source_root ||= Pathname.new(File.expand_path("../../", __FILE__))
-  end
-
-  I18n.load_path << File.expand_path("locales/en.yml", source_root)
-  I18n.reload!
-
 end
+
+I18n.load_path << File.expand_path("../../locales/en.yml", __FILE__)
+I18n.reload!
 
 require "vagrant-bindfs/vagrant/plugin"
