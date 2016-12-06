56ec5e0f0b080a3d1d08fdc51b5a3ddb540fa086
Paul dG <pdesgarets@users.noreply.github.com>
2016-12-06 12:09:19 +0100

    Fix when input is a symbol
    I don't know if it should also be applied to other shellescape "just in case".

--- lib/vagrant-bindfs/vagrant/capabilities/linux/system_checks.rb
+++ lib/vagrant-bindfs/vagrant/capabilities/linux/system_checks.rb
@@ -8,14 +8,14 @@ module VagrantBindfs
             def bindfs_exists_user(machine, user)
               (
                 user.nil? || \
-                machine.communicate.test("getent passwd #{user.shellescape}")
+                machine.communicate.test("getent passwd #{user.to_s.shellescape}")
               )
             end
 
             def bindfs_exists_group(machine, group)
               (
                 group.nil? || \
-                machine.communicate.test("getent group #{group.shellescape}")
+                machine.communicate.test("getent group #{group.to_s.shellescape}")
               )
             end
           end
